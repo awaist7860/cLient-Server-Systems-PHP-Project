2680c6a7a9469dc6186c2e2b921328ab17e41ca1
awaist7860 <48568803+awaist7860@users.noreply.github.com>
2020-11-07 23:26:13 +0000

    All the classes updated for gettting users

--- AwaisAuctions/Models/AuctionDataSet.php
+++ AwaisAuctions/Models/AuctionDataSet.php
@@ -1,7 +1,27 @@
 <?php
 
+require_once ('Models/Database.php');
+require_once ('Models/AuctionData.php');
 
 class AuctionDataSet
 {
+    protected $_dbHandle, $_dbInstance;
 
+    public function __construct() {
+        $this->_dbInstance = Database::getInstance();
+        $this->_dbHandle = $this->_dbInstance->getdbConnection();
+    }
+
+    public function fetchAllStudents() {
+        $sqlQuery = 'SELECT * FROM users';
+
+        $statement = $this->_dbHandle->prepare($sqlQuery); // prepare a PDO statement
+        $statement->execute(); // execute the PDO statement
+
+        $dataSet = [];
+        while ($row = $statement->fetch()) {
+            $dataSet[] = new AuctionData($row);
+        }
+        return $dataSet;
+    }
 }
