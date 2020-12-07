3a58755299ffbbda1ff553597a6c1c4321b34286
awaist7860 <48568803+awaist7860@users.noreply.github.com>
2020-12-07 10:01:39 +0000

    Login Button Half Works
    Need to fix login button and logout button

--- AwaisAuctions/Models/Login.php
+++ AwaisAuctions/Models/Login.php
@@ -1,6 +1,8 @@
 <?php
 require_once 'Database.php';
-require_once '../controllers/Login.php';
+require_once 'userLogin.php';
+
+//require_once '../controllers/Login.php';
 require_once '../Views/template/header.phtml';
 
 
@@ -8,27 +10,42 @@ class Login
 {
     protected $_dbConnection, $_dbInstance;
 
-
     public function __construct()
     {
         $this->_dbInstance = Database::getInstance();
         $this->_dbConnection = $this->_dbInstance->getDbConnection();
     }
 
+        public function getAllUsers($name) {
+
+            $sqlQuery = "SELECT * FROM users WHERE username = ?";     //Sql injection for security
+
+            $statement = $this->_dbHandle->prepare($sqlQuery); // prepare a PDO statement
+            $statement->execute(array($name)); // execute the PDO statement
+
+            $dataSet = [];
+            while ($row = $statement->fetch()) {
+                array_push($dataSet, new userModel($row));
+            }
+            return $dataSet;
+        }
 
 
-    //public function login($email, $psw)
-        public function login()
-    {
-        $Username = $_POST['username'];
-        $Password = $_POST['password'];
-        echo $_REQUEST['username'];
-        echo $_REQUEST['password'];
-        $UsernameQuery = 'SELECT username FROM users WHERE username = $Username AND password = $Password';
 
-        return $UsernameQuery;
+//    //public function login($email, $psw)
+//        public function login()
+//    {
+//        $Username = $_POST['username'];
+//        $Password = $_POST['password'];
+//        echo $_REQUEST['username'];
+//        echo $_REQUEST['password'];
+//        $UsernameQuery = 'SELECT username FROM users WHERE username = $Username AND password = $Password';
+//
+//        if($UsernameQuery = true){
+//            return "Sql Query Worked";
+//        }
+//
+//        return $UsernameQuery;
 
-
-    }
 
 }
--- AwaisAuctions/controllers/Login.php
+++ AwaisAuctions/controllers/Login.php
@@ -1,21 +1,54 @@
 <?php
+
+include_once 'Models/LoginPage.php';
 include_once '../Models/Login.php';
 include_once '../Views/template/header.phtml';
 
+session_start();
 
-$email = $_POST['username'];
-$pasw = $_POST['password'];
-$login = new Login();
 
-$result = $login->login($email, $pasw);
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-if($result)
-{
-    echo "welcome";
-//    header("location: ../header.php");
+//$userDataSet = new userDataSet();
+//$Login = new Login();
+
+$loginPage = new userLogin();
+
+$result = $loginPage-> login($username, $password);
+
+print_r($result);
+
+if ($result) {
+    header("Location: ../index.php");
+} else {
+    header("Location: ../index.php?error=Invalid details");
 }
 
-else
-{
-    echo "Wrong details!, please retry!";
-}
+//$result = $loginPage->login($email, $pasw);
+
+
+
+
+
+
+
+
+//
+//$login = new Login();
+//
+//
+//
+//if($result)
+//{
+//    echo "welcome";
+////    header("location: ../header.php");
+//}
+//
+//else
+//{
+//    echo "Wrong details!, please retry!";
+//}
+
+
+
